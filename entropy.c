c531359d12d8beb978611912c9ae0a843e2d5fff
Robert David Graham <robert_david_graham@yahoo.com>
2018-12-17 19:04:29 -0500

    entropy

--- entropy.c
+++ entropy.c
@@ -3,130 +3,237 @@
 #include <stdlib.h>
 
 #include <unistd.h>
-#include <sys/wait.h>
 
+
+/**
+ * This is a list of the tests that we run. You can change the order of tests
+ * in this list in order to change both the order in which they are calculated
+ * and the order they are printed.
+ */
+const char *test_names[] = {
+    "exec",
+    "libc",
+    "heap",
+    "mmap",
+    "stack",
+    "static",
+    0
+};
+
+/**
+ * An array of results, where the result at each index corresponds to the 
+ * test named in the `test_names` variable.
+ */
+struct test_results
+{
+    unsigned long long values[6];
+};
+
+struct spawned
+{
+    pid_t pid;
+    int fd[2];
+};
+
+
+/**
+ * Count the number of bits set to '1' in a bitmask.
+ */
 unsigned
 count_bits(unsigned long long n) 
 { 
-  unsigned count = 0; 
+    unsigned count = 0; 
   
-  while (n) {
-    count += n & 1; 
-    n >>= 1ULL; 
-  } 
-  return count; 
+    while (n) {
+        count += n & 1; 
+        n >>= 1ULL; 
+    } 
+    return count; 
 }
 
-unsigned long long
-spawn_program(const char *prog, const char *type)
+/**
+ * Do a fork()/exec() to spawn the program
+ */
+static struct spawned
+spawn_program(const char *progname, const char *argument)
 {
-    unsigned long long result = 0;    
-    int fd[2];
-    pid_t pid;
-
+    struct spawned child = {0};
+    
     /* Create a pipe to get output from child */
-    pipe(fd);
+    pipe(child.fd);
 
     /* Spawn child */
-    pid = fork();
+    child.pid = fork();
 
-    if (pid == 0) {
-        /* If we are the child, then execute a new program */
+    if (child.pid == 0) {
+        /* We are the CHILD */
         char * new_argv[3];
-        new_argv[0] = (char *)prog;
-        new_argv[1] = (char *)type;
+        new_argv[0] = (char *)progname;
+        new_argv[1] = (char *)argument;
         new_argv[2] = 0;
-        close(fd[0]);
-        dup2(fd[1], 1);
-        execve(prog, new_argv, 0);
+
+        /* Close the 'read' end of the pipe, since child only writes to it */
+        close(child.fd[0]);
+
+        /* Set the 'write' end of the pipe 'stdout' */
+        dup2(child.fd[1], 1);
+
+        /* Now execute our child with new program */
+        execve(progname, new_argv, 0);
     } else {
-        /* If we are the parent, grab the results */
-        char buf[1024];
-        close(fd[1]);
-        FILE *fp = fdopen(fd[0], "rt");
-        if (fgets(buf, sizeof(buf), fp)) {
-            result = strtoull(buf, 0, 0);
-        }
+        /* We are the PARENT */
+
+        /* Close the 'write' end of the pipe, since parent only reads
+         * from it */
+        close(child.fd[1]);
     }
+    return child;
+}
+
+/**
+ * Reads input from child and parses the results
+ */
+static struct test_results
+parse_results(struct spawned *child)
+{   
+    struct test_results result = {0};
+    char buf[1024];
+    FILE *fp;
+    
+    /* Convert 'fd' file descript to 'FILE*' pointer so I can use 'fgets()' */
+    fp = fdopen(child->fd[0], "rt");
+
+    /* Get the line of output from the child program */
+    if (fgets(buf, sizeof(buf), fp)) {
+        size_t j;
+        char *p = buf;
+
+        for (j = 0; j<sizeof(result.values)/sizeof(result.values[0]); j++)
+            result.values[j] = strtoull(p, &p, 0);
+    }
+    fclose(fp);
+    close(child->fd[0]);
+
     return result;
 }
 
-void
-run_tests(const char *progname, const char *testname, size_t loop_count)
+/**
+ * This spawns a bunch of child programs, which will print their locations.
+ * This then collects the results and prints them.
+ * @param progname
+ *      The name of the current program (from argv[0]), so that we simply
+ *      spawn ourselves.
+ * @param loop_count
+ *      The number of programs to spawn, the more programs, the more
+ *      accurate the results.
+ */
+static void
+run_tests(const char *progname, size_t loop_count)
 {
     size_t i;
-    unsigned long long *results = calloc(loop_count, sizeof(*results));;
-    unsigned long long mask = 0;
-    size_t outstanding = 0;
+    struct test_results *results = calloc(loop_count, sizeof(*results));;
+    struct test_results mask = {0};
     
+    /* Spawn many processes and store their results */
     for (i=0; i<loop_count; i++) {
-        results[i] = spawn_program(progname, testname);
-        if (results[i] == 0) {
+        struct spawned child;
+
+        /* Launch a child program */
+        child = spawn_program(progname, "testall");
+        if (child.pid == 0) {
             fprintf(stderr, "error\n");
             abort();
-        } else
-            outstanding++;
-
-        /* Only allow up to so many outstanding at a time, to avoid
-         * overloading small devices with too many processes */
-        while (outstanding > 16) {
-            wait(0);
-            outstanding--;
         }
+
+        /* Get results from child */
+        results[i] = parse_results(&child);
     }
     
-    /* Wait until all children have exited */
-    while (outstanding) {
-        wait(0);
-        outstanding--;
-    }
 
-    /* Calculate results */
+    /* Calculate results. We are looking for a bitmask that reflects all the
+     * bits that have changed from one run to the next. This is probably
+     * a stupid O(n^2) algorithm that compares every result against every
+     * other, I could probably do this more efficiently if I spent two
+     * seconds engaging my brain and thinking about this. */
     for (i=1; i<loop_count; i++) {
-        unsigned long long x = results[i-1] ^ results[i];
-        mask |= x;
+        size_t j;
+        for (j=0; j<sizeof(mask.values)/sizeof(mask.values[0]); j++) {
+            unsigned long long x;
+            x = results[i-1].values[j] ^ results[i].values[j];
+            mask.values[j] |= x;
+        }
     }
 
-    printf("%-8s 0x%016llx %02u-bits\n", testname, mask, count_bits(mask));
+    /* Print a header indicative of how bit the addresses can get,
+     * which is typically around 40 bits for 64-bit systems */
+    if (sizeof(size_t) == 8)
+        printf("%-8s 0x%016llx %02u-bits\n", "-max-", 0xEEddccbbaaULL, 40);
+    else
+        printf("%-8s 0x%016llx %02u-bits\n", "-max-", 0xddccbbaaULL, 32);
+
+    /* Print which bits are involved in the page, which is usually 12 bits
+     * on major systems (x86, x64, ARM, etc.). Most random layout, except
+     * the stack, is at the page boundary */
+    printf("%-8s 0x%016llx %02u-bits\n", "-page-", 0xFFFULL, 12);
+
+    /* Print the results. For each NAMED test, we print both the MASK
+     * reflecting which bits randomly changed, and also the COUNT of
+     * the number of bits. */
+    for (i=0; i<sizeof(mask.values)/sizeof(mask.values[0]); i++) 
+        printf("%-8s 0x%016llx %02u-bits\n", 
+                test_names[i], 
+                mask.values[i], 
+                count_bits(mask.values[i]));
+}
+
+/**
+ * Prints pointers to things that rely in different memory segments.
+ * Each time we run this program, different values should be
+ * printed.
+ */
+static void
+print_layout(void)
+{
+    size_t i;
+    for (i=0; test_names[i]; i++) {
+        const char *name = test_names[i];
+        if (strcmp(name, "exec") == 0) {
+            printf("%p ", (void *)print_layout);
+        } else if (strcmp(name, "libc") == 0) {
+            printf("%p ", (void *)sprintf);
+        } else if (strcmp(name, "heap") == 0) {
+            printf("%p ", malloc(5));
+        } else if (strcmp(name, "mmap") == 0) {
+            printf("%p ", malloc(5000000));
+        } else if (strcmp(name, "stack") == 0) {
+            printf("%p ", (void *)alloca(5));
+        } else if (strcmp(name, "static") == 0) {
+            printf("%p ", (void *)"static");
+        } else
+            printf("%p ", (void*)0);
+    }
+    printf("\n");
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc == 2 && strcmp(argv[1], "exec") == 0) {
-        printf("%p\n", main);
+    if (argc == 2 && strcmp(argv[1], "testall") == 0) {
+        print_layout();
         return 0;
-    } else if (argc == 2 && strcmp(argv[1], "libc") == 0) {
-        printf("%p\n", sprintf);
-        return 0;
-    } else if (argc == 2 && strcmp(argv[1], "heap") == 0) {
-        printf("%p\n", malloc(5));
-        return 0;
-    } else if (argc == 2 && strcmp(argv[1], "mmap") == 0) {
-        printf("%p\n", malloc(5000000));
-        return 0;
-    } else if (argc == 2 && strcmp(argv[1], "stack") == 0) {
-        printf("%p\n", alloca(5));
-        return 0;
-    } else if (argc == 2 && strcmp(argv[1], "static") == 0) {
-        printf("%p\n", "static");
-        return 0;
-    } else if (argc == 1 || argc == 2 && atoi(argv[1]) > 0) {
+    } else if (argc == 1 || (argc == 2 && atoi(argv[1]) > 0)) {
         int count = 100;
+
+        /* Parse command line options */
         if (argc == 2)
             count = atoi(argv[1]);
 
-        fprintf(stderr, "This can take a while: %d\n", count); fflush(stderr);
-        if (sizeof(size_t) == 8)
-            printf("%-8s 0x%016llx %02u-bits\n", "-max-", 0xEEddccbbaaULL, 40);
-        else
-            printf("%-8s 0x%016llx %02u-bits\n", "-max-", 0xddccbbaaULL, 32);
-        printf("%-8s 0x%016llx %02u-bits\n", "-page-", 0xFFFULL, 12);
-        run_tests(argv[0], "stack", count);
-        run_tests(argv[0], "heap", count);
-        run_tests(argv[0], "mmap", count);
-        run_tests(argv[0], "exec", count);
-        run_tests(argv[0], "static", count);
-        run_tests(argv[0], "libc", count);
+        /* Automatically reap zombies */
+        if (signal(SIGCHLD, SIG_IGN) == SIG_ERR) {
+            perror("signal(SIGCHLD, SIG_IGN)");
+            exit(1);
+        }
+
+        run_tests(argv[0], count);
     } else {
         fprintf(stderr, "usage: just run with no parameters\n");
     }
